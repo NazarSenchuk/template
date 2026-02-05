60b60938bd260d7a82bbc2ec65ece5966296c3c6
NazarSenchuk <113719257+NazarSenchuk@users.noreply.github.com>
2026-02-05 14:13:01 +0200

    Fixes of cicd (#2)
    * Update1
    
    * Update API message response to 'Hello from backend3!'
    
    * Update 3
    
    * Update 4
    
    * commit 5
    
    * Update 6
    
    * Update 7
    
    * Final Update
    
    * dwa

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // Simple API endpoint
 app.get('/api/message', (req, res) => {
   res.json({ 
-    message: 'Hello from backend26!',
+    message: 'Hello from backend27!',
     timestamp: new Date().toISOString()
   });
 });
