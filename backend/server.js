7d9498a44d8edcdb5e05e43100b008907c40dd46
NazarSenchuk <senchuknazar6@gmail.com>
2026-02-06 12:40:53 +0200

    dwa

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.get('/api/message', (req, res) => {
   res.json({ 
 
-    message: 'Hello from backend67withscanig!',
+    message: 'Hello from backend67withscanidg!',
 
     timestamp: new Date().toISOString()
   });
