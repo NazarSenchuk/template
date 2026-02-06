8fb63d5bbbb8b8e7900b93d4004eb856f70b8979
NazarSenchuk <senchuknazar6@gmail.com>
2026-02-06 11:52:29 +0200

    added scanning

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // Simple API endpoint
 app.get('/api/message', (req, res) => {
   res.json({ 
-    message: 'Hello from backend67!',
+    message: 'Hello from backend67withscaning!',
     timestamp: new Date().toISOString()
   });
 });
